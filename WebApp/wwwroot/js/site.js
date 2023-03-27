22e76ed1ba40283c7989b08c9d4f240858039ac3
MiaChensSV <miachensv@gmail.com>
2023-03-27 14:04:18 +0200

    Add project files.

---
+++ WebApp/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿const toggleClass = (element, className, compareValue) => {
+    let isFixed = false;
+    let _element = document.querySelector(element);
+    if(value<=compareValue)
+        isFixed = true
+    else isFixed=false
+    _element.classList.toggle(className,isFixed)
+    }
+toggleClass("footer","fixed",document.body.scrollHeight,(window.innerHeight-_element.scrollHeight))
+
